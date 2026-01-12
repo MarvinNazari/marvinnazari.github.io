c5c15c9cbd338f1fecec78bceca9de8e3a30acc8
Marvin Nazari <marvin.nazari@bontouch.com>
2026-01-12 19:33:29 +0100

    Add custom domain wavio.co

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://marvinnazari.github.io",
+  site: "https://wavio.co",
   integrations: [mdx(), sitemap(), tailwind()],
 });
